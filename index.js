b4b5f56d88917bdd61c90396a95cb560b4eaf1bc
Rashonda Edwards <rashondaedwards@aol.com>
2023-05-12 14:32:12 -0700

    adding the 404 page

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Ran this in terminal: npm i dotenv
+require('dotenv').config()
+
 // Require express
 const express = require("express")
 
@@ -14,7 +17,12 @@ app.get('/second', (req,res)=>{
     res.send('Hi Computer')
 })
 
+// 404 Error Route needs to go below other routes!!
+app.get('*', (req,res)=>{
+    res.status(404).send("<h1>404 Page</h1>")
+})
+
 // Keep server open to listen for connections (callback is optional)
-app.listen((3002), ()=>{
+app.listen((process.env.PORT), ()=>{
     console.log("... I'm listening...")
 })
